957330d935ddc7f63051ed4fe1e2b32fa3440aa4
Cephas <nguyenvananh19201@gmail.com>
2021-06-19 10:56:33 +0700

    Initial commit

---
+++ app/src/main/java/com/example/recylerviewroom/User.java
@@ -0,0 +1,57 @@
+package com.example.recylerviewroom;
+
+public class User {
+    private String name;
+    private int age;
+    private String phone;
+    private String mail;
+    private String address;
+
+    public User(String name, int age, String phone, String mail, String address) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        this.mail = mail;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
